e4a8d71260b878974eb9a86cbf6733f97630caa5
cawoesies <viskaramadhani20@gmail.comm>
2025-06-16 08:13:30 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProdukController;
 use App\Http\Controllers\Api\UserController;
 
-// -------------------- ROUTE PRODUK --------------------
 Route::get('/produk', [ProdukController::class, 'index']);
 Route::get('/produk/{id}', [ProdukController::class, 'show']);
 Route::post('/produk', [ProdukController::class, 'store']);
 Route::put('/produk/{id}', [ProdukController::class, 'update']);
 Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
 
-// -------------------- ROUTE USER --------------------
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::post('/user', [UserController::class, 'store']);
