b462ac46eb5babdae832d45d253556e6d15ddabd
cosuxma <selvianaputri2008@gmail.com>
2025-06-16 08:23:01 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,33 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProdukController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PesananController;
 
-Route::get('/produk', [ProdukController::class, 'index']);
-Route::get('/produk/{id}', [ProdukController::class, 'show']);
-Route::post('/produk', [ProdukController::class, 'store']);
-Route::put('/produk/{id}', [ProdukController::class, 'update']);
-Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
+// ---------------- ROUTE PRODUK ----------------
+Route::prefix('produk')->group(function () {
+    Route::get('/', [ProdukController::class, 'index']);
+    Route::get('/{id}', [ProdukController::class, 'show']);
+    Route::post('/', [ProdukController::class, 'store']);
+    Route::put('/{id}', [ProdukController::class, 'update']);
+    Route::delete('/{id}', [ProdukController::class, 'destroy']);
+});
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::post('/user', [UserController::class, 'store']);
-Route::put('/user/{id}', [UserController::class, 'update']);
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
+// ---------------- ROUTE USER ----------------
+Route::prefix('user')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+// ---------------- ROUTE PESANAN ----------------
+Route::prefix('pesanan')->group(function () {
+    Route::get('/', [PesananController::class, 'index']);
+    Route::get('/{id}', [PesananController::class, 'show']);
+    Route::post('/', [PesananController::class, 'store']);
+    Route::put('/{id}', [PesananController::class, 'update']);
+    Route::delete('/{id}', [PesananController::class, 'destroy']);
+});
